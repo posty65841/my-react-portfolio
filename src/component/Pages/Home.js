d1312df89def306888643725d67d76a6ec8c0fdd
austin <posty65841@gmail.com>
2022-11-05 13:37:55 -0500

    read me added

--- src/component/Pages/Home.js
+++ src/component/Pages/Home.js
@@ -11,7 +11,8 @@ export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      <p>
+      <h2>Austin Post</h2>
+      <p > 
         <img src={require("../../assets/8220B1F4-07AE-427F-BD37-7B4671ABAD64_1_105_c.jpeg")} alt="my logo" width="100%" height="100%" />
       </p>
       <footer style={styles.footer}> <a target="_blank" href="https://github.com/posty65841?tab=repositories">
