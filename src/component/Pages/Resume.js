a3e4138c6fc1aacafa0830c1a43219ddcc85e0af
austin <posty65841@gmail.com>
2022-11-07 14:23:05 -0600

    pushing up

--- src/component/Pages/Resume.js
+++ src/component/Pages/Resume.js
@@ -16,7 +16,7 @@ function Resume() {
            
 
                 <h2>Resume</h2>
-             <p >
+             <p>click to view resume
                 <a style={styles.Resume}
                  target="_blank" href="https://drive.google.com/file/d/1wGna016rueNkSVdt-_BeWcU5FYS5vADU/view?usp=sharing">
                 <img src={require("../../assets/Screen Shot 2022-11-07 at 10.47.52.png")} alt="resume img" width="50%" height="50%" />
